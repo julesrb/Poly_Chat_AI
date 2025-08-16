073db8243559ce13e4fb9b978ce5083f7ddbb516
Jules <jbjulesbernard3@gmail.com>
2025-08-16 16:02:38 +0200

    react: fold unfold categorires

--- poly_chat_ai/src/app/Catergories/Categories.tsx
+++ poly_chat_ai/src/app/Catergories/Categories.tsx
@@ -20,38 +20,47 @@ interface CategoriesProps {
 }
 
 function Categories({ id, title, color, threads, setConversationSelection }: CategoriesProps) {
+
   const bgClass = colorClassesBright[color] || "bg-gray-300";
+  const [isOpen, setIsOpen] = useState(true);
   function handleNewthread() {
     // This function can be implemented to add new threads if needed
   }
+
+  const toggleThreads = () => {
+    setIsOpen(!isOpen);
+  };
 
   return (
     <div>
       <div
         key={id}
         className={`px-4 py-1 mb-1 rounded ${bgClass} font-bold rounded-full hover:bg-gray-100 cursor-pointer flex items-center justify-between`}
+		onClick={toggleThreads}
       >
         {title}
-        <button
+        {/* <button
           onClick={() => handleNewthread()}
           className=" hover:bg-blue-600 font-bold rounded-full ml-4 px-2"
         >
           +
-        </button>
+        </button> */}
       </div>
-      <div className="space-y-2 flex-1 overflow-auto">
-        {threads.map((thr) => (
-          <Threads
-            key={thr.id}
-            id={thr.id}
-            title={thr.title}
-            color={color}
-            conversations={thr.conversations}
-			categoryId={id}
-			setConversationSelection={setConversationSelection}
-          />
-        ))}
-      </div>
+      {isOpen && (
+        <div className="space-y-2 flex-1 overflow-auto">
+          {threads.map((thr) => (
+            <Threads
+              key={thr.id}
+              id={thr.id}
+              title={thr.title}
+              color={color}
+              conversations={thr.conversations}
+              categoryId={id}
+              setConversationSelection={setConversationSelection}
+            />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
